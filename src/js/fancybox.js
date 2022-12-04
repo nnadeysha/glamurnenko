bc4114504ab4cca3d26cd58467f0d27b409f161e
novikova.n_qschool <nadeysha23@gmail.com>
2022-12-04 16:14:35 +0400

    fix: fix gallery section

--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -2,6 +2,6 @@ import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox.css";
 
 
-Fancybox.bind("[data-fancybox]", {
+Fancybox.bind("[data-fancybox='gallery']", {
     // Your options go here
   })
